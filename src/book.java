5e26a676340f0710056dcf39f71ee397ccfc7240
JanekAgent <jasiusko1@gmail.com>
2022-12-18 13:17:20 +0100

    added constructor to book

--- src/book.java
+++ src/book.java
@@ -1,11 +1,18 @@
 public class book {
+    Library library;
     String title;
     int id;
     int year;
     int edition;
     Author author;
-    book(string title){
-        
+    public
+    book(Library libraryl,String titlel,int yearl,int editionl,Author authorl){
+        this.library=libraryl;
+        this.title=titlel;
+        this.year=yearl;
+        this.edition=editionl;
+        this.author=authorl;
     }
+
     
 }
