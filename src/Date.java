9cb30a95c5d8bb51831b06321037836a97f1154d
JanekAgent <jasiusko1@gmail.com>
2022-12-18 18:59:16 +0100

    added string dates and dateToString

--- src/Date.java
+++ src/Date.java
@@ -7,4 +7,15 @@ public class Date {
         this.month=monthl;
         this.year=yearl;
     }
+    Date(String datel){
+        String[] dateListed = datel.split("/");
+        this.year=Integer.valueOf(dateListed[0]);
+        this.month=Integer.valueOf(dateListed[1]);
+        this.day=Integer.valueOf(dateListed[2]);
+    }
+    String dateToString(){
+        String str;
+        str=Integer.toString(year)+"/"+Integer.toString(month)+"/"+Integer.toString(day);
+        return str;
+    }
 }
